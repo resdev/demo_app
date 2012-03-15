1c64f067034f8ff81bad35d86250027c7ce3f903
resdev@bridgeware.net <resdev@bridgeware.net>
2012-03-15 16:45:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '966f351609e2a00eaccbf169e8cb4a503d5e3944f7fa35f4157c92772f3476a4d3d39270dc0f5fb0ad8d5e0ab1f54e0bc5c70a7b2922d4537d16975d9bfa2abe'
